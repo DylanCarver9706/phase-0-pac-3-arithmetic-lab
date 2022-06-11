3174c602bead8dd8cd8e5d116b52a6d9517c8dec
Dylan <dylancarver14@gmail.com>
2022-06-11 16:02:01 -0500

    commit tree 64b77ff6d31331254728db7707a7d97fd5b56300

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a, b){
+    return a + b
+
+}
+
+function subtract(b, a){
+    return b - a
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
+
+function increment(n){
+    return (++n)
+}
+
+function decrement(n){
+    return (--n)
+}
+
+function makeInt(string){
+    parseInt(string, 10)
+    parseInt(string, 0)
+    return (string)
+
+}
+
+function preserveDecimal(string){
+    parseFloat(string)
+    return parseFloat(string, 10)
+}
